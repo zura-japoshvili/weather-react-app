f96af96bdb0fb59bd1275f1f73da8430178fb440
zura-japoshvili <japoshvili777@gmail.com>
2022-01-24 19:16:16 +0400

    #1 - stating project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
+const api = {
+  key: "201058e832fa52692296c02c11766767",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
     </div>
   );
 }
